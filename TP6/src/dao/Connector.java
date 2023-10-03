c05516e448e87d1fbf9a7210f154f69663d0dd3e
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2023-10-03 18:33:48 -0300

    Finalizar clase Connector

--- TP6/src/dao/Connector.java
+++ TP6/src/dao/Connector.java
@@ -1,30 +1,28 @@
 package dao;
 
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 public class Connector implements IConnector {
 	
-	// Clase estática que almacena los nombres de las bases de datos más usadas.
+	/**
+	 * Clase que contiene constantes para los nombres de las bases de datos.
+	 * Proporciona acceso a los nombres de las bases de datos utilizados en la aplicación.
+	 */
 	public static class DB {
 		public static final String bdregistro = "bdregistro";
 		public static final String bdPersonas = "bdPersonas";
 	}
 	
-	// Propiedades
 	public ConnectorSettings settings;
 	public String database;
 	
-	
-	// Constructores
 	public Connector(ConnectorSettings data, String database) {
 		this.settings = data;
 		this.database = database;
@@ -42,7 +40,10 @@ public class Connector implements IConnector {
 		this(ConnectorSettings.DEFAULT, DB.bdPersonas);
 	}
 
-	// Métodos
+	/**
+	 * Construye el URI para conectar a la base de datos.
+	 * @return El URI para conectar a la base de datos.
+	 */
 	private String buildURI() {
 		return this.settings.host 
 				+ this.database 
@@ -54,6 +55,9 @@ public class Connector implements IConnector {
 				+ (this.settings.useSSL ? "true" : "false");
 	}
 	
+	/**
+	 * Abre una nueva conexión al servidor de base de datos.
+	 */
 	@Override
 	public Connection openConnection() throws SQLException {
 		Connection connection = null;
@@ -65,6 +69,13 @@ public class Connector implements IConnector {
         return connection;
 	}
 
+	/**
+	 * Realiza una consulta que devuelve un conjunto de datos.
+	 * Está diseñado para ser usado con consultas de tipo SELECT.
+	 * @param query La consulta a ser ejecutada.
+	 * @param parameters Los parámetros, en caso de usarse.
+	 * @returns Objeto TransactionResponse con el resultado de la operación.
+	 */
 	@Override
 	public TransactionResponse<Dict> fetch(String query, Object[] parameters) throws SQLException {
 		TransactionResponse<Dict> finalResult = new TransactionResponse<Dict>();
@@ -107,35 +118,104 @@ public class Connector implements IConnector {
 	        }
 	    }
 	}
-
+	
+	/**
+	 * Realiza una consulta que devuelve un conjunto de datos.
+	 * Está diseñado para ser usado con consultas de tipo SELECT.
+	 * @param query La consulta a ser ejecutada.
+	 * @param parameters Los parámetros, en caso de usarse.
+	 * @returns Objeto TransactionResponse con el resultado de la operación.
+	 */
 	@Override
 	public TransactionResponse<Dict> fetch(String query, Dict parameters) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+        Object[] params = parameters.getParameters(query);
+		query = query.replaceAll("@\\w+", "?");
+	    return fetch(query, params);
 	}
 
+	/**
+	 * Realiza una consulta que devuelve un conjunto de datos.
+	 * Está diseñado para ser usado con consultas de tipo SELECT.
+	 * @param query La consulta a ser ejecutada.
+	 * @returns Objeto TransactionResponse con el resultado de la operación.
+	 */
 	@Override
 	public TransactionResponse<Dict> fetch(String query) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+        Object[] params = new Object[] {};
+	    return fetch(query, params);
 	}
 
+	/**
+	 * Realiza una consulta que no devuelve un conjunto de datos.
+	 * Está diseñado para usarse con consultas de tipo INSERT, UPDATE, DELETE.
+	 * @param query La consulta a ejecutar.
+	 * @param parameters Los parámetros, en caso de usarse.
+	 * @return Objeto TransactionResponse con el resultado de la operación.
+	 */
 	@Override
 	public TransactionResponse<?> transact(String query, Object[] parameters) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	    Connection cn = null;
+	    TransactionResponse<?> t = TransactionResponse.create();
+	    try {
+	        cn = this.openConnection();
+	        cn.setAutoCommit(false); // Desactivamos el modo de autocommit para empezar la transacción
+	        
+	        PreparedStatement ps = cn.prepareStatement(query);
+	        
+	        if (parameters != null) {
+	            for (int i = 0; i < parameters.length; i++) {
+	                ps.setObject(i + 1, parameters[i]);
+	            }
+	        }
 
+	        int rowsAffected = ps.executeUpdate();
+	        cn.commit();
+	        t.rowsAffected = rowsAffected;
+	         // ÉXITO
+	    } catch (SQLException e) {
+	        if (cn != null) {
+	            cn.rollback(); // En caso de excepción, DESHACER CAMBIOS EN EL DB
+	        }
+	        t.dbError = e;
+	        e.printStackTrace();
+	        throw e;
+	    } finally {
+	        try {
+	            if (cn != null) {
+	                cn.close();
+	            }
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+	    }
+	    return t;
+	}
+	
+	/**
+	 * Realiza una consulta que no devuelve un conjunto de datos.
+	 * Está diseñado para usarse con consultas de tipo INSERT, UPDATE, DELETE.
+	 * @param query La consulta a ejecutar.
+	 * @param parameters Los parámetros, en caso de usarse.
+	 * @return Objeto TransactionResponse con el resultado de la operación.
+	 */
 	@Override
 	public TransactionResponse<?> transact(String query, Dict parameters) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		Object[] params = parameters.getParameters(query);
+		query = query.replaceAll("@\\w+", "?");
+		return transact(query, params);
 	}
-
+	
+	/**
+	 * Realiza una consulta que no devuelve un conjunto de datos.
+	 * Está diseñado para usarse con consultas de tipo INSERT, UPDATE, DELETE.
+	 * @param query La consulta a ejecutar.
+	 * @param parameters Los parámetros, en caso de usarse.
+	 * @return Objeto TransactionResponse con el resultado de la operación.
+	 */
 	@Override
 	public TransactionResponse<?> transact(String query) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		Object[] params = new Object[] {};
+		return transact(query, params);
 	}
 
 }
--- TP6/src/dao/Dict.java
+++ TP6/src/dao/Dict.java
@@ -1,10 +1,64 @@
 package dao;
 
 import java.util.HashMap;
-import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Dict extends HashMap<String, Object> {
 
 	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * Conocer si un elemento existe en la lista a partir de su key.
+	 * @param key La clave por la cual se busca el elemento en la lista.
+	 * @return true, si existe. False, si no existe.
+	 */
+	public boolean exists(String key) {
+		for(String v : keySet()) {
+			if(v.equals(key)) return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * Cuenta los parámetros presentes en una consulta SQL.
+	 * @param str La consulta SQL a ser analizada
+	 * @return La cantidad de parámetros presentes en la consulta.
+	 */
+	public static int countKeys(String str) {
+		int z = 0;
+		Pattern pattern = Pattern.compile("@\\w+");
+		Matcher matcher = pattern.matcher(str);
+		while(matcher.find()) {
+			z++;
+		}
+		return z;
+	}
+	
+	/**
+	 * Analiza una consulta SQL y extrae los parámetros de esa consulta que están en forma de \@key. 
+	 * @param query La consulta SQL a ser analizada.
+	 * @return Array con los valores correspondientes a los parámetros, en el orden en el que aparecen en la consulta.
+	 */
+	public Object[] getParameters(String query) {
+        Pattern pattern = Pattern.compile("@(\\w+)");
+        Matcher matcher = pattern.matcher(query);
+        int coincidences = countKeys(query);
+        Object[] params = new Object[coincidences];
+        int i = 0;
+        while (matcher.find()) {
+            String paramName = matcher.group(1);
+            boolean e = exists(paramName);
+            if(e) {
+            	Object val = get(paramName);
+            	params[i] = val;
+            	
+            } else {
+            	// Lanzar una Exception
+            }
+            i++;
+        }
+        return params;
+	}
 	
 }
