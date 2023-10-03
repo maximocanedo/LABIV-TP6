4f9082fc70e709366235e0cff59139f3cde6bac8
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2023-10-02 22:06:57 -0300

    Usar estilos predeterminados de Windows

--- TP6/src/main/Principal.java
+++ TP6/src/main/Principal.java
@@ -1,5 +1,8 @@
 package main;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 import negocio.PersonaNegocio;
 import negocioImpl.PersonaNegocioImpl;
 import presentacion.controlador.Controlador;
@@ -9,7 +12,21 @@ import presentacion.vista.Menu;
 public class Principal {
 
 	public static void main(String[] args) {
-			
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+	    } 
+	    catch (UnsupportedLookAndFeelException e) {
+	    	e.printStackTrace();
+	    }
+	    catch (ClassNotFoundException e) {
+	    	e.printStackTrace();
+	    }
+	    catch (InstantiationException e) {
+	    	e.printStackTrace();
+	    }
+	    catch (IllegalAccessException e) {
+	    	e.printStackTrace();
+	    }
 		Menu ventana = new Menu();
 		PersonaNegocio negocio = new PersonaNegocioImpl();
 		Controlador controlador = new Controlador(ventana, negocio);
